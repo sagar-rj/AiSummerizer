d5468e601b84429c286dd981b27fdc6b034bb7f0
Sagar <sgrraj1845@gmail.com>
2025-08-17 03:23:10 +0530

    connected to backend

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ marked.setOptions({
 
 
 // Define the server URL
-const API_URL = `${import.meta.env.VITE_API_URL}/api`
+const API_URL = `${import.meta.env.VITE_API_URL}`
 
 function App() {
     const apiKey = import.meta.env.VITE_TINY_MCE_API_KEY;
